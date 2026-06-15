f8d08957c3b9ee470df35a3170f528a4c675f14c
Eric Rhys Taylor <support@radialtimeline.com>
2026-06-15 09:13:02 -0700

    backup: 2026-06-15 16:13:02

--- scripts/copy-to-vault.mjs
+++ scripts/copy-to-vault.mjs
@@ -4,8 +4,8 @@ import path from "node:path";
 const ROOT = process.cwd();
 const TARGET_DIRS = [
 	"/Users/ericrhystaylor/Obsidian Vault Author/.obsidian/plugins/editorialist",
-	"/Users/ericrhystaylor/Documents/RT LLC/CodeBase/Obsidian Vault Jane Austen/.obsidian/plugins/editorialist",
-	"/Users/ericrhystaylor/Documents/RT LLC/CodeBase/Obsidian Vault Sherlock Holmes/.obsidian/plugins/editorialist",
+	"/Users/ericrhystaylor/Documents/RT LLC/Plugin/Test Vaults/Obsidian Vault Jane Austen/.obsidian/plugins/editorialist",
+	"/Users/ericrhystaylor/Documents/RT LLC/Plugin/Test Vaults/Obsidian Vault Sherlock Holmes/.obsidian/plugins/editorialist",
 ];
 const TARGET_LABEL = "/Author/ and /Jane Austen/ and /Sherlock Holmes/";
 const FILES_TO_COPY = ["manifest.json", "main.js", "styles.css"];
